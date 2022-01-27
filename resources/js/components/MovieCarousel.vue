91570680550f7dd382ef385ce9afe6784f0c6f44
cincenfrank <fpetrangeli1@gmail.com>
2022-01-27 17:35:50 +0100

    created carousel

---
+++ resources/js/components/MovieCarousel.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="row">
+    <div class="col-8">
+      <img class="w-100" src="" alt="" />
+    </div>
+    <div class="col-4"></div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>

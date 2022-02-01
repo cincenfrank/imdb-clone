a211fbfc59b00d87642fce16529f2e050d8da1bf
alfredochs <chanta.alfredo@gmail.com>
2022-02-01 12:25:41 +0100

    push backend

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -14,13 +14,12 @@ class MovieController extends Controller
      */
     public function index()
     {
-        $movies = config('Movies');
-        $data = [];
-        foreach ($movies as $index => $movie) {
-            $movie['id'] = $index;
-            $data[] = $movie;
-        }
-        return view('welcome', compact('Movies'));
+        $movies = config('Movies.php');
+        // foreach ($movies as $index => $movie) {
+        //     $movie['id'] = $index;
+        //     $data[] = $movie;
+        // }
+        return view('welcome', ['movies' => $movies]);
         // Modifica Welcome
     }
 

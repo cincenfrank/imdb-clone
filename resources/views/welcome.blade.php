44a803a050ba7d0bba8573124e5e3b294bb6df81
alfredochs <chanta.alfredo@gmail.com>
2022-02-01 16:36:30 +0100

    slider done

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,11 @@
 
     <script src="{{ asset('js/app.js') }}" defer></script>
 
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    </script>
     <title>Laravel</title>
 
     <!-- Fonts -->
@@ -76,7 +80,7 @@
     </div>
 
 
-    <div class="flex-center position-ref full-height">
+    {{-- <div class="flex-center position-ref full-height">
         @if (Route::has('login'))
             <div class="top-right links">
                 @auth
@@ -107,7 +111,7 @@
                 <a href="https://github.com/laravel/laravel">GitHub</a>
             </div>
         </div>
-    </div>
+    </div> --}}
 </body>
 
 </html>

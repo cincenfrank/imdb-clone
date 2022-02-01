faf66e892b6016bdd7d3e4846548d13a2a69d0e0
LeonardoCeccarelli <leo.ceccarelli@tiscali.it>
2022-02-01 17:01:00 +0100

    change model movie and category for many to many relationship

--- app/Movie.php
+++ app/Movie.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Movie extends Model
 {
-    //
+    public function categories()
+    {
+        return $this->belongsToMany("App\Category");
+    }
 }
